4d1738b0e1eec787cc896b6d210fe57635092f28
arcbjorn <arky.happ@gmail.com>
2022-10-30 17:54:14 -0300

    feat(Tailwind): extend config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -8,5 +8,12 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      config: { applyBaseStyles: false },
+    }),
+    preact(),
+  ],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,35 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line no-undef
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      "2xs": "325px",
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
+
+    extend: {
+      fontFamily: {
+        "plex-mono": ["IBMPlexMono", "Courier New", "Courier", "monospace"],
+        "plex-sans": ["IBMPlexSans", "sans-serif", "sans"],
+      },
+
+      animation: {
+        appear: "fadeIn 1s ease-in-out",
+      },
+
+      keyframes: () => ({
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 100 },
+        },
+      }),
+    },
+  },
+  plugins: [],
+};
