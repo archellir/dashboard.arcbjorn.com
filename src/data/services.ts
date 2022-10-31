643f4fd8d52aa47f6121b7b3346e6e5b16bdb25a
arcbjorn <arky.happ@gmail.com>
2022-10-30 22:35:41 -0300

    feat(data): add services and stack items

---
+++ src/data/services.ts
@@ -0,0 +1,78 @@
+import type { IServiceItem } from "./types";
+
+export const externalServices: IServiceItem[] = [
+  {
+    name: "Blog",
+    description: "Built with Deno, Fresh, Preact, TS",
+    url: "https://blog.arcbjorn.com",
+    monitorId: 2,
+  },
+  {
+    name: "Personal website",
+    description: "Build with Gatsby.js, GraphQL, TS",
+    url: "https://arcbjorn.com",
+    monitorId: 8,
+  },
+  {
+    name: "Tax calculator",
+    description: "Build with Vue 3, Pinia, TS",
+    url: "https://tax.arcbjorn.com",
+    monitorId: 3,
+  },
+  {
+    name: "This dashboard",
+    description: "Built with Astro, Preact, TS",
+    url: "/",
+    monitorId: 9,
+  },
+  {
+    name: "Web monitoring",
+    description: "built on Uptime-kuma",
+    url: "https://uptime.arcbjorn.com/status",
+  },
+];
+
+export const internalServices: IServiceItem[] = [
+  {
+    name: "Service Logger",
+    description: "built on Dozzle",
+    url: "https://dozzle.dev/",
+    monitorId: 11,
+  },
+  {
+    name: "Company website",
+    description: "SvelteKit, TS",
+    url: "/",
+    monitorId: 10,
+  },
+  {
+    name: "Analytics dashboard",
+    description: "built on Umami",
+    url: "https://umami.is/",
+    monitorId: 7,
+  },
+  {
+    name: "Database interface",
+    description: "built on pgAdmin",
+    url: "https://www.pgadmin.org/",
+    monitorId: 6,
+  },
+  {
+    name: "Server infrastructure",
+    description: "built on Portainer",
+    url: "https://www.portainer.io/",
+    monitorId: 5,
+  },
+  {
+    name: "Git server",
+    description: "built on Gitea",
+    url: "https://gitea.io/",
+    monitorId: 4,
+  },
+  {
+    name: "Server web interface",
+    description: "built on FileBrowser",
+    url: "https://filebrowser.org/",
+    monitorId: 1,
+  },
+];
---
+++ src/data/stackItems.ts
@@ -0,0 +1,38 @@
+import { AnsibleIcon } from "../components/icons/AnsibleIcon";
+import { DockerIcon } from "../components/icons/DockerIcon";
+import { PortainerIcon } from "../components/icons/PortainerIcon";
+import { GithubActionsIcon } from "../components/icons/GithubActionsIcon";
+import { LockIcon } from "../components/icons/LockIcon";
+import { DenoIcon } from "../components/icons/DenoIcon";
+import { CloudflareIcon } from "../components/icons/CloudflareIcon";
+
+export const stackItems = [
+  {
+    name: "Portainer",
+    icon: PortainerIcon,
+  },
+  {
+    name: "Github Actions",
+    icon: GithubActionsIcon,
+  },
+  {
+    name: "Caddy v2",
+    icon: LockIcon,
+  },
+  {
+    name: "Docker & Docker Compose",
+    icon: DockerIcon,
+  },
+  {
+    name: "Ansible",
+    icon: AnsibleIcon,
+  },
+  {
+    name: "Cloudflare",
+    icon: CloudflareIcon,
+  },
+  {
+    name: "Deno Deploy",
+    icon: DenoIcon,
+  },
+];
